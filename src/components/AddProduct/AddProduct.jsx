ad5e7e5ef46aad27c1c6a27edb4452979b2ebde7
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-02-26 21:50:44 +0600

    addproduct added

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
 
 const AddProduct = () => {
+
+    const handleAddProduct = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const image = form.image.value;
+        const name = form.name.value;
+        const brand = form.brand.value;
+        const type = form.type.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const rating = form.rating.value;
+
+        console.log(image, name, brand, type, price, description, rating);
+    }
+
     return (
-        <div>
-            <h2>Add Product</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Add Product!</h1>
+                    <p className="py-6">Adding a product involves inputting its details like name, description, price, and images into a system, ensuring accuracy and completeness. This step is crucial for online platforms, ensuring customers can find and purchase items seamlessly. Accuracy and clarity are paramount for a smooth user experience.</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleAddProduct} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Image</span>
+                            </label>
+                            <input type="text" name='image' placeholder="image" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Brand Name</span>
+                            </label>
+                            <input type="text" name='brand' placeholder="brand" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Type</span>
+                            </label>
+                            <input type="text" name='type' placeholder="laptop/phone/other" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Price</span>
+                            </label>
+                            <input type="text" name='price' placeholder="price" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Short Description</span>
+                            </label>
+                            <input type="text" name='description' placeholder="description" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Rating</span>
+                            </label>
+                            <input type="text" name='rating' placeholder="out of 5" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Add Product</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
